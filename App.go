886227eb71e1b82a5e0f0b9584c61348dae71536
Phan Đức Việt <viet@techmaster.vn>
2017-08-09 02:35:48 +0700

    load env at init

--- App.go
+++ App.go
@@ -4,17 +4,20 @@ import (
 	"github.com/kataras/iris"
 	"github.com/paduvi/BasicIrisExample/middlewares"
 	"github.com/paduvi/BasicIrisExample/controllers"
-	"github.com/joho/godotenv"
 	"os"
 	"github.com/paduvi/BasicIrisExample/httputils"
 	"strconv"
+	"github.com/joho/godotenv"
 )
 
-func main() {
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
 	}
+}
+
+func main() {
 	app := iris.New()
 
 	app.OnErrorCode(iris.StatusInternalServerError, middlewares.ErrorHandler)
